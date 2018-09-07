5d1ad549608b156f0415b8c82c9189491e420f60
Aamir Virani <1476954+aamirv@users.noreply.github.com>
2018-09-06 23:27:38 -0700

    made into PhotoCleaner class

--- PhotoCleaner.py
+++ PhotoCleaner.py
@@ -2,6 +2,65 @@ import os
 import re
 from datetime import datetime
 
+class PhotoCleaner:
+
+  def __init__(self, root="/Users/aamir/Dropbox (Personal)/Photos"):
+    if root is None:
+      pass # should quit and say not allowed
+
+    # if root is not legal directory should quit
+
+    self.root = root
+    self.dir_to_skip = None
+  
+  def load_dir_to_skip(self):
+    # if a file exists then load it
+    # else make it None
+    self.dir_to_skip = None
+  
+  def create_dir_to_skip(self, ask_user=True):
+    self.dir_to_skip = {}
+    for dir_name, _, _ in os.walk(self.root):
+      if dir_name.startswith("/Users/aamir/Dropbox (Personal)/Photos/Aamir Virani - 4048"):
+        self.dir_to_skip[dir_name] = True
+      if "Originals" in dir_name:
+        self.dir_to_skip[dir_name] = True
+      else:
+        skip = True
+        if ask_user:
+          text = input('Skip {}? [n]'.format(dir_name))
+          skip = 'y' in text or 'Y' in text
+        
+        self.dir_to_skip[dir_name] = skip
+  
+  def start(self):
+    self.load_dir_to_skip()
+    if self.dir_to_skip is None:
+      self.create_dir_to_skip()
+
+    for k,v in self.dir_to_skip.items():
+      if v:
+        print('skipping {}'.format(k))
+      else:
+        process_directory(k)
+  
+def process_directory(dir_name):
+  (folder_date, folder_location) = parse_directory_name(dir_name)
+  print(folder_date, folder_location)
+  # get photo list from this directory
+  # for each photo p
+    # process_photo(p, d, l)
+
+def process_photo(file_name):
+  pass
+  # parse createddate from exif
+  # parse takendate from exif
+  # parse location from exif
+  # update createddate for file using exif (i.e., return to old value)
+  # update takendate for exif from file
+  # if location does not exist or is default value
+    # update location for exif from user
+  
 def parse_directory_name(dir_name):
   date_result = None
   location_result = None
@@ -22,30 +81,10 @@ def parse_directory_name(dir_name):
     index_starting_descriptor = time_match.span()[0]+ time_match.span()[1]
     if index_starting_descriptor < len(folder_name):
       location_result = folder_name[index_starting_descriptor:].lstrip().rstrip()
+      # l = convert to gps_coordinates?
 
   return (date_result, location_result)
 
-  # location_string
-  # l = convert to gps_coordinates?
-  # return (d, l)
-
-rootDir = ("/Users/aamir/Dropbox (Personal)/Photos")
-for dirName, subdirList, fileList in os.walk(rootDir):
-  if dirName.startswith("/Users/aamir/Dropbox (Personal)/Photos/Aamir Virani - 4048") or "Originals" in dirName:
-    continue
-  
-  (folder_date, folder_location) = parse_directory_name(dirName)
-
-## process_directory(dir)
-  # (default_date, default_location) = parse_directory_name(dir)
-  # for each photo p
-    # process_photo(p, d, l)
-
-## process_photo(file)
-    # parse createddate from exif
-    # parse takendate from exif
-    # parse location from exif
-    # update createddate for file using exif (i.e., return to old value)
-    # update takendate for exif from file
-    # if location does not exist or is default value
-      # update location for exif from user
+if __name__ == "__main__":
+  pc = PhotoCleaner()
+  pc.start()
